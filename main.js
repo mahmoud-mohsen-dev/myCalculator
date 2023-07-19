ff1c6994c3676ea612a319a1157266712d68eedb
mody120 <42476566+mody120@users.noreply.github.com>
2023-07-19 06:15:52 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,104 +1,130 @@
-let container = document.querySelector(".operations")
-let display = document.querySelector(".display span")
-let calc = document.querySelector("#calc")
-let reset = document.querySelector("#reset")
-let del = document.querySelector("#del")
-let inputs = document.querySelectorAll('.operations .input')
+class Calculator {
+    constructor(currentOperandTextElement, previousOperandTextElement){
+        this.currentOperandTextElement = currentOperandTextElement
+        this.previousOperandTextElement = previousOperandTextElement
+        this.clear()
+    }   
 
-let numsArr = [];
-let operationArr = ['/','x','-','+'];
-let nums ;
-let totalArr = [];
-let result;
+    clear (){
+        this.currentOperand = ''
+        this.previousOperand = ''
+        this.operation = undefined
+    }
 
-inputs.forEach(ele => {
-    ele.addEventListener('click',function(e) {
-        let ops = e.target.getAttribute('value');
-        if (!operationArr.includes(ops)){
-            numsArr.push(ops)
-            nums = numsArr.join("")            
-            display.textContent = nums
+    delete (){
+        this.currentOperand = this.currentOperand.toString().slice(0,-1)
+    }
+
+    appendNumber (number){
+        if (number === '.' && this.currentOperand.includes('.')) return
+        this.currentOperand = this.currentOperand.toString() + number.toString()
+    }
+
+    chooseOperation (operation){
+        if (this.currentOperand === '') return
+        if (this.previousOperand !== '') {
+            this.calculate()
         }
-        if (operationArr.includes(ops)) {
-            numsArr = []
-            display.textContent = ops
-            totalArr.push(nums, ops)
-        } 
-    })
+        this.operation = operation
+        this.previousOperand = this.currentOperand
+        this.currentOperand = ''
+    }
+
+    calculate (){
+        let result
+        let prev = parseFloat(this.previousOperand)
+        let current = parseFloat(this.currentOperand)
+
+        if (isNaN(prev) || isNaN(current)) return
+
+        switch (this.operation){
+            case '+':
+                result = prev + current
+                break
+
+            case '-':
+                result = prev - current
+                break
+
+            case 'x':
+                result = prev * current
+                break
+
+            case '÷':
+                result = prev / current
+            break
+            default:
+                break
+        }
+
+        this.currentOperand = result
+        this.previousOperand = ''
+        this.operation = undefined
+    }
+
+    getDisplayNumber (number){
+        const stringNumber = number.toString()
+        const integerDigits = parseFloat(stringNumber.split('.')[0])
+        const decimalDigits = stringNumber.split('.')[1]
+        let integerDisplay
+        if (isNaN(integerDigits)) {
+            integerDisplay = ''
+        } else {
+            integerDisplay = integerDigits.toLocaleString('en', {maximumFractionDigits: 0})
+        }
+        if (decimalDigits != null){
+            return `${integerDisplay}.${decimalDigits}`
+        } else {
+            return integerDisplay
+        }
+    }
+
+    updateDisplay (){
+        this.currentOperandTextElement.innerText = 
+        this.getDisplayNumber(this.currentOperand)
+
+        if (this.operation !== undefined){
+            this.previousOperandTextElement.innerText = 
+            `${this.getDisplayNumber(this.previousOperand)} ${this.operation}`
+        } else {
+            this.previousOperandTextElement.innerText = ''
+        }
+    }
+
+}
+
+const previousOperand = document.querySelector('[data-previous-operand]')
+const currentOperand = document.querySelector('[data-current-operand]')
+const operationButtons = document.querySelectorAll('[data-operation]')
+const numberButtons = document.querySelectorAll('[data-number]')
+const deleteButton = document.querySelector('[data-delete]')
+const equalButton = document.querySelector('[data-equals]')
+const allClearButton = document.querySelector('[data-all-clear]')
+
+
+const calculator = new Calculator(currentOperand, previousOperand)
+
+numberButtons.forEach(button => button.addEventListener('click', () => {
+    calculator.appendNumber(button.innerText)
+    calculator.updateDisplay()
+}))
+
+operationButtons.forEach(button => button.addEventListener('click', () => {
+    calculator.chooseOperation(button.innerText)
+    calculator.updateDisplay()
+}))
+
+equalButton.addEventListener('click', () => {
+    calculator.calculate()
+    calculator.updateDisplay()
 })
 
+allClearButton.addEventListener('click', () => {
+    calculator.clear()
+    calculator.updateDisplay()
+})
 
-
-del.onclick = () => {
-    let content = display.textContent;
-    let contentArr = content.split('');
-    if (contentArr.length > 1){
-        contentArr.pop()
-        nums = contentArr.join('')
-        numsArr = []
-        result = nums;
-        display.textContent = nums
-    } else {
-        nums = '0'
-        display.textContent = '0'
-    }
-}
-
-reset.onclick = () => {
-    numsArr = [];
-    nums = '';
-    totalArr = []
-    display.textContent = '0';
-}
-
-
-calc.onclick = function() {
-
-
-    numsArr = []
-    totalArr.push(nums)
-    console.log('totalArr: ' + totalArr)
-    let before ;
-    let after ;
-
-    totalArr.forEach((e,i,arr) => {
-        if (operationArr.includes(e)){
-            // console.log(result)
-            if (result === undefined || result === null){
-                before = arr[i - 1];
-                after = arr[i + 1];
-                console.log('before: ' + before)
-                console.log('after: ' + after)
-                
-            } else {
-                before = result
-                after = arr[i + 1];
-                console.log('before else: ' + before)
-                console.log('after else: ' + after)
-            }
-
-            switch (e) {
-                case '+':
-                    result = +before + +after;
-                    break;
-                case '-':
-                    result = +before - +after;
-                    break;
-                
-                case 'x':
-                    result = +before * +after;
-                    break;
-                
-                case '/':
-                    result = +before / +after;
-                    break;
-            
-                default:
-                    break;
-            }
-            console.log("result : " + result)
-        }
-    })
-    display.textContent = result;
-}
-
+deleteButton.addEventListener('click', () => {
+    calculator.delete()
+    calculator.updateDisplay()
+})
